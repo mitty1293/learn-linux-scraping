05a11407b88b74cad3a46d79b3cb72a429ddf2b4
mitty1293 <k.fmichi@gmail.com>
2021-06-20 04:10:18 +0900

    waitの調整

--- requests_to_slack/main.py
+++ requests_to_slack/main.py
@@ -3,8 +3,8 @@ import requests
 url = 'http://www.usupi.org/sysad/backno.html'
 rg = requests.get('http://splash:8050/render.html',
                 params={
-                    'url':url, 
-                    'wait': 2
+                    'url': url,
+                    'wait': 3
                 }
 )
 print(rg.text)
