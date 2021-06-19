64e9cd6d0a0f541805a12d5b7640168ad9c399ec
mitty1293 <k.fmichi@gmail.com>
2021-06-20 04:03:28 +0900

    main.py修正

--- requests_to_slack/main.py
+++ requests_to_slack/main.py
@@ -1,1 +1,10 @@
 import requests
+
+url = 'http://www.usupi.org/sysad/backno.html'
+rg = requests.get('http://splash:8050/render.html',
+                params={
+                    'url':url, 
+                    'wait': 2
+                }
+)
+print(rg.text)
