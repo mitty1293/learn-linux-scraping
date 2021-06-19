4122ef126fd18e7dc685fd1946c9c12e7d2d3429
mitty1293 <k.fmichi@gmail.com>
2021-06-20 04:22:42 +0900

    return_rended_pageメソッド完成

--- requests_to_slack/main.py
+++ requests_to_slack/main.py
@@ -1,10 +1,16 @@
 import requests
 
-url = 'http://www.usupi.org/sysad/backno.html'
-rg = requests.get('http://splash:8050/render.html',
-                params={
-                    'url': url,
-                    'wait': 3
-                }
-)
-print(rg.text)
+def return_rended_page():
+    url = 'http://www.usupi.org/sysad/backno.html'
+    rg = requests.get('http://splash:8050/render.html',
+                    params={
+                        'url': url,
+                        'wait': 3
+                    }
+    )
+    print(rg.text)
+    with open('rended.html', 'wb') as f:
+        f.write(rg.content)
+
+def extract_urls():
+    return 0
