3935f4304ab78702c24d489ee2b82c4493b6fca2
mitty1293 <k.fmichi@gmail.com>
2021-06-21 03:45:09 +0900

    slack messageの整形

--- requests_to_slack/main.py
+++ requests_to_slack/main.py
@@ -19,10 +19,33 @@ def extract_urls():
 
 def to_slack():
     requests.post(settings.WEBHOOK_URL,
-                data = json.dumps({
-                    'text': u'from py'
-                })
-    )
+                data = json.dumps(
+                    {
+                        "blocks": [
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": "Today's topic:\n*<http://www.usupi.org/sysad/294.html|Vol.294 - 共有ライブラリをざっくり理解する (レベル:中級)>*"
+                                }
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": "Previous:\n*<http://www.usupi.org/sysad/294.html|Vol.294 - 共有ライブラリをざっくり理解する (レベル:中級)>*"
+                                }
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": "Next:\n*<http://www.usupi.org/sysad/294.html|Vol.294 - 共有ライブラリをざっくり理解する (レベル:中級)>*"
+                                }
+                            }
+                        ]
+                    }
+                ))
 
 def main():
     return_rended_page()
